c3c39186417a42bbd5f080ab0bb0f8d145bebe46
krishnavetthi <krishnavetthib@outlook.com>
2023-02-06 23:08:59 -0500

    first commit

---
+++ task3Output.js
@@ -0,0 +1,8 @@
+var header = document.querySelector("header");
+var element = header.getElementsByTagName("li")[2];
+element.querySelector('a').textContent="Projects";
+
+let para=document.querySelector("p");
+let span=para.querySelectorAll("span");
+span[1].textContent="an Employee";
+
